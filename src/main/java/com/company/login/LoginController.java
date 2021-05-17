f053f3a87cc1fbc4ec2f7796d5445a3db6a7f0cf
cms02 <minoui1234@gmail.com>
2021-05-18 00:21:53 +0900

    security-context.xml - 접근 불가 페이지를 핸들링하기 위해 <http> 태그 사이에 코드 한 줄을 추가 LoginController.java 생성

---
+++ src/main/java/com/company/login/LoginController.java
@@ -0,0 +1,16 @@
+package com.company.login;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class LoginController {
+	private static final Logger logger = LoggerFactory.getLogger(LoginController.class);
+
+	@RequestMapping(value = "/login/accessDenied.do")
+	public String accessDeniedPage() throws Exception {
+		return "/login/accessDenied";
+	}
+}
